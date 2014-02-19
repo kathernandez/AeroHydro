18da14f146ab0bef6335c2bd861df05d15e9ff06
kathernandez <hernandezkatheryne@gmail.com>
2014-02-19 13:14:09 -0500

    Functions are defined for velocity components and stream function

--- Infinite_Row_of_Vortices.py
+++ Infinite_Row_of_Vortices.py
@@ -15,4 +15,17 @@ x=np.linpace(xstart,xend,N)  # x direnction 1D array
 y=np.linspace(ystart,yend,N) # y direction 1D array 
 X,Y=np.meshgrid(x,y)         # mesh grid created 
 
+gamma = 5.0                  #strength of vortex (same for all) 
+
+#define funtion to compute the velocity components
+def getVelocityVortex=(strength,xv,yv,X,Y):
+    u = + strength/(2*pi)*(Y-yv)/((X-xv)**2+(Y-yv)**2)
+    v = - strength/(2*pi)*(X-xv)/((X-xv)**2+(Y-yv)**2)
+    return u,v
+
+#define function to compute the stream function     
+def getStreamFunctionVortex=(strength,xv,yv,X,Y):
+    psi = strength/(4*pi)*np.log((X-xv)**2+(Y-yv)**2)
+    return psi 
+
 
