d9e12ee0c3a8acdc8c68031b505d36ac3d6b3a86
kathernandez <hernandezkatheryne@gmail.com>
2014-02-26 15:37:57 -0500

    Flow around a cylinder with radius and stag points plotted

--- VortexLift.py
+++ VortexLift.py
@@ -6,9 +6,9 @@ from math import *
 N=50                           #number of points 
 xstart, xend = (-2.0,2.0)     # x-dir boundaries
 ystart, yend = (-1.0,1.0)     #y-dir boundaries 
-x = linspace.np(xstart,xend,N)  #1D array in x-dir
-y = linspace.np(ystart,yend,N)  #1D array in y-dir
-X,Y = meshgrid.np(x,y)
+x = np.linspace(xstart,xend,N)  #1D array in x-dir
+y = np.linspace(ystart,yend,N)  #1D array in y-dir
+X,Y = np.meshgrid(x,y)
 
 k = 1   #strength of doublet 
 xdoublet, ydoublet = 0.0, 0.0 #location of the doublet
@@ -24,5 +24,45 @@ def getVelocityDoublet(strength, xd, yd, X, Y):
 def getStreamFunctionDoublet (strength, xd, yd, X, Y):
     psi = -strength/(2*pi)* (Y-yd)/((X-xd)**2+(Y-yd)**2)
     return psi
+
+#compute flow around a cylinder by adding free stream 
+
+udoublet, vdoublet = getVelocityDoublet(k, xdoublet, ydoublet, X, Y)
+psidoublet = getStreamFunctionDoublet(k, xdoublet, ydoublet, X, Y)
+
+#compute free stream velocity components and psi
+
+ufreestream = np.ones((N,N), dtype = float)
+vfreestream = np.zeros((N,N), dtype = float)
+psifreestream = uinf*Y
+
+#apply superposition to create flow around a cylinder 
+
+u = ufreestream + udoublet
+v = vfreestream + vdoublet
+psi = psidoublet + psifreestream
+
+
+#Plotting 
+size = 10
+plt.figure(figsize=(size, (yend-ystart)/(xend-xstart)*size))
+plt.xlim(xstart,xend)
+plt.ylim(ystart,yend)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.streamplot(X,Y,u,v,\
+        density = 2.0, linewidth = 1, arrowsize =1, arrowstyle = '->')
+plt.scatter(xdoublet, ydoublet, c='m', s=80, marker = 'o')
+
+#plot the radius of cylinder 
+R=sqrt(k/(2*pi*uinf))
+circle = plt.Circle((0,0),radius = R, color='m', alpha = 0.5)
+plt.gca().add_patch(circle)
+
+#plot stagnation points 
+xstag1, ystag1 = + R, 0
+xstag2, ystag2 = -R, 0
+plt.scatter([xstag1,xstag2],[ystag1,ystag2], c ='g', s=80, marker ='o')
+plt.show()
 
 
