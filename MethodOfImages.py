e35c33e582fc65f558e86eb74a4e616585a51d83
kathernandez <hernandezkatheryne@gmail.com>
2014-03-06 02:04:11 -0500

    vortex near a wall
    created a new class for calculating u,v, and psi of a vortex. code runs
    but produces a plot of vortex imposed over source near wall

--- MethodOfImages.py
+++ MethodOfImages.py
@@ -13,6 +13,7 @@ x = np.linspace(xstart, xend, N)  #X 1D array
 y = np.linspace(ystart, yend, N)  #y 1D array
 X,Y = np.meshgrid(x,y)            #generate mesh
 
+#--------------------source near a wall -----------------------------
 class Source: 
     def __init__(self,strength,x,y):
         self.strength = strength
@@ -52,7 +53,55 @@ plt.streamplot(X,Y,u,v,\
 plt.scatter(source.x,source.y,c='r',s=80,marker='o')
 plt.scatter(sourceimage.x,sourceimage.y,c='r',s=80,marker='D')
 plt.axhline(0.0,color='k',linestyle='--',linewidth=4)
+#plt.show()
+
+#--------------------------vortex near a wall------------------
+
+class Vortex:
+    def __init__(self,strength,x,y):
+        self.strength = strength
+        self.x,self.y = x,y
+    #get velocity field
+    def velocity(self,X,Y):
+        self.u = +self.strength/(2*pi)*(Y-self.y)/((X-self.x)**2+(Y-self.y)**2)
+        self.v = -self.strength/(2*pi)*(X-self.x)/((X-self.x)**2+(Y-self.y)**2)
+    #get stream function
+    def streamfunction(self,X,Y):
+        self.psi = -self.strength/(4*pi)*np.log((X-self.x)**2+(Y-self.y)**2)
+
+strengthvortex = 1.0
+xvortex,yvortex = 0.0,0.5
+
+vortex = Vortex(strengthvortex,xvortex,yvortex)
+vortex.velocity(X,Y)
+vortex.streamfunction(X,Y)
+
+vorteximage=Vortex(-strengthvortex,xvortex,-yvortex)
+vorteximage.velocity(X,Y)
+vorteximage.streamfunction(X,Y)
+
+#apply superposition
+u = vortex.u + vorteximage.u
+v = vortex.v + vorteximage.v
+psi = vortex.psi + vorteximage.psi
+
+#plotting
+size = 10
+plt.figure(num=0,figsize=(size,(yend-ystart)/(xend-xstart)*size))
+plt.xlabel('x', fontsize =16)
+plt.ylabel('y', fontsize = 16)
+plt.xlim(xstart,xend)
+plt.ylim(ystart,yend)
+plt.streamplot(X,Y,u,v,\
+        density=2.0, linewidth=1,arrowsize=1,arrowstyle='->')
+plt.scatter(vortex.x,vortex.y, c='m',s=80,marker='o')
+plt.scatter(vorteximage.x,vorteximage.y,c='m',s=80,marker='D')
+plt.axhline(0.0,color='k',linestyle='--',linewidth=4)
 plt.show()
+
+
+
+
 
         
 
